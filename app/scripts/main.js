d538ad4dbd7e3077a93eecd3889ff630237eab8d
James Greenaway <jamesgreenaway@gmail.com>
2013-05-14 13:56:21 +0100

    Initial commit, holding page.

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global console*/
+
+require.config({
+    paths: {
+        jquery: '../components/jquery/jquery'
+    },
+    shim: {
+    }
+});
+
+
+require(['app', 'jquery'], function (app, $) {
+    'use strict';
+    // use app here
+
+    console.log(app);
+    console.log('Running jQuery %s', $().jquery);
+});
