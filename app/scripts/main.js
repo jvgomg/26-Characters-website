24aaedc4bb75890d14c8d351568b90a80f79c064
James Greenaway <james@james-mbp.local>
2013-05-26 13:16:51 +0100

    Split up h2 titles. Added page fade in. vunit poly fill. A bunch of inital styling.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,10 +9,25 @@ require.config({
 });
 
 
-require(['app', 'jquery'], function (app, $) {
+require(['jquery'], function ($) {
     'use strict';
     // use app here
 
-    console.log(app);
-    console.log('Running jQuery %s', $().jquery);
+    $(function() {
+        // JS is enabled... page is hidden
+
+
+        // Split award into 2 lines
+        var $award = $('#header .award'),
+            $newAwardType = $('<h2>').insertAfter($award),
+            $awardType = $award.find('abbr').appendTo($newAwardType);
+
+
+        // Fade page in
+        var $body = $('body').removeClass('fadedOut').addClass('fadeIn');
+    });
+
+
+
+
 });
